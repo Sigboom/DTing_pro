8ad925bbe02bd6a9922945240ed53a0303fd0aeb
Sigboom <34954447+Sigboom@users.noreply.github.com>
2019-04-10 10:38:13 +0800

    Update piShow.py

--- piShow.py
+++ piShow.py
@@ -15,7 +15,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import copy
 
-def drawOutLine(ax, times):
+#马青公式
+def pi(n):
+    p = 10 ** (n + 10)
+    a = p * 16 // 5
+    b = p * 4 // -239
+    f = a + b
+    p = f
+    j = 3
+    while abs(f):
+        a //= -25
+        b //= -57121
+        f = (a + b) // j
+        p += f
+        j += 2
+    return p // 10**10  # 去掉 k 位，k=10
+
+def draw_out_line(ax, times):
     M = 1000
     color = np.zeros(M)
     line_r = (times * 2 + 210) * np.ones(M)
@@ -29,35 +45,32 @@ def drawOutLine(ax, times):
     plt.ylim((0, (times * 2 + 210)))
     plt.grid(False)
 
-def drawpoint(ax, num, times, ch):
+def draw_point(ax, pi, times):
     # 生成外围颜色
-    drawOutLine(ax, times)
+    draw_out_line(ax, times)
     #取出所有位
     x = []
     for i in range(times):
-        temp, addnum = divmod(num * 10 ** i, 10)
+        temp, addnum = divmod(pi(i), 10)
         x.append(np.pi / 5 * int(addnum))
 
-    r = 125 * np.ones(times) + list(2 * i for i in range(0, times))
+    r = times / 2.5 * np.ones(times) + list(2 * i for i in range(0, times))
     color = copy.deepcopy(x) #确定颜色
     for i in range(1, len(x) - 1):
         x[i] = x[i] + (x[i - 1] + x[i + 1] - 10) * (np.pi / 100)
     theta = x
-    area = (r / (np.pi * 5)) ** 2
+    area = (r / (np.pi * 2)) ** 2
     ax.scatter(theta, r, c=color, s=area, cmap='hsv', alpha=0.5)
 
-    plt.text(np.pi * 1.2, 40 + times / 5, ch, size=90, wrap=True)
-
 def main():
-    N = 300
-    num = np.pi # or np.e
-    ch = 'π' # or e
-
-    print("start to draw %lf points!" % np.e)
+    N = 1000
+    ch = 'π'
+    print("start to draw %lf points!" % np.pi)
     plt.style.use('dark_background')
     fig, ax = plt.subplots()
     ax = plt.subplot(111, polar=True)
-    drawpoint(ax, num, N, ch)
+    draw_point(ax, pi, N)
+    plt.text(np.pi * 1.2, 80 + N / 4, ch, size=90, wrap=True)
     fig.set_size_inches(10, 10)
     plt.show()
 
