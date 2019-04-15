7ee097cab702a7c3874ba26b951cf40105abc512
Sigboom <sigboom@163.com>
2019-04-15 18:22:46 +0800

    add main.cpp

---
+++ docs/docs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  docs
+//
+//  Created by Daniel on 2019/4/15.
+//  Copyright © 2019 Daniel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
